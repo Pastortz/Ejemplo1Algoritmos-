f4effa9dd4ce061c500acbc52a44a7601ef971ff
alber <alber@AlbertoTzita>
2025-08-02 17:02:59 -0600

    "canvios 02-08-2025"

---
+++ src/guihub/Guihub.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guihub;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alber
+ */
+public class Guihub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre,edad;
+        Scanner lector = new Scanner(System.in);
+        nombre = lector.next();
+        System.out.println("Ingrese su edad : ");
+        edad = lector.next();
+        
+        
+        System.out.println("Ingrese su Nombre : ");
+    }
+    
+}
